47a197a4e1c2f620bd61426e31a2b4101cd078c0
dan <inscissor@gmail.com>
2022-10-02 14:01:30 -0400

    Added more packages.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 import { getFileList } from './common/files.js'
 
-/*
-npm i moment
-npm i glob
-*/
+
+// import { simpleGit } from 'simple-git';
+
+// const options = {
+//   baseDir: process.cwd(),
+//   binary: 'git',
+//   maxConcurrentProcesses: 6,
+//   trimmed: false
+// };
+
+//const git = simpleGit(options);
+// // https://github.com/steveukx/git-js
+// let o = (await git.log()).all;  
+// console.table(o);
+
 
 async function main() {
 
-  const result = await getFileList("**/*sa.js");
-  
-  result.forEach(file => {
-    console.log(file);
-  });
-  
+  // const result = await getFileList("**/*sa.js");  
+  // result.forEach(file => {
+  //   console.log(file);
+  // });
+  //console.log(git.log());
+
+
 }
 
 // Giddy up
